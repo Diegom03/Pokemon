1eb25e45c79425a5e375f30f964642ab30faf65a
Diegom03 <diegomartinm03@gmail.com>
2023-02-21 13:03:59 +0100

    commit

--- Pokemon_1/app/src/main/java/com/example/pokemon_api_01/lst_pokemons/view/LstPokemonAdapter.java
+++ Pokemon_1/app/src/main/java/com/example/pokemon_api_01/lst_pokemons/view/LstPokemonAdapter.java
@@ -1,16 +1,43 @@
 package com.example.pokemon_api_01.lst_pokemons.view;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pokemon_api_01.R;
 
 public class LstPokemonAdapter {
     /* VIEW HOLDER */
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class LstPokemonAdapter extends RecyclerView.Adapter<LstPokemonAdapter.ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).
+                    inflate(R.layout.item_pokemon, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            Pokemon p = dataset.get(position);
+            holder.nombreTextView.setText(p.getName());
+
+            Glide.with(context)
+                    .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + p.getNumber() + ".png")
+                    .centerCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(holder.fotoImageView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
 
         private ImageView fotoImageView;
         private TextView nombreTextView;
